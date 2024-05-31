f60a4f4360deb77e5c1d9e5b4fc44d29ca53ec1c
EOTAEGYU <wkdrnxorb@naver.com>
2024-05-31 17:25:42 +0900

    token

---
+++ jwt/src/test/java/com/example/jwt/JwtApplicationTests.java
@@ -0,0 +1,42 @@
+package com.example.jwt;
+
+import com.example.jwt.service.JwtService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Autowired
+	private JwtService jwtService;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void tokenCreate(){
+		var claims = new HashMap<String, Object>();;
+		claims.put("user_id", 923);
+
+		var expiredAt = LocalDateTime.now().plusMinutes(10);
+
+		var jwtToken = jwtService.create(claims, expiredAt);
+
+		System.out.println(jwtToken);
+	}
+
+	@Test
+	void tokenValidation(){
+		var token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo5MjMsImV4cCI6MTcxNzE0MzQ0N30.flB13scp0QQO_BzHVz7OnLaCVPyoHZp_CaJzmRaZ-Qk";
+		jwtService.validation(token);
+
+	}
+
+
+}
